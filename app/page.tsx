adb21a8e1fba3553e3a21602f62c47cf64a363cc
Mehdi <mehdi.havaei77@gmail.com>
2023-09-26 14:34:20 +0330

    implement base of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,42 @@
+"use client";
 import Image from "next/image";
+import { assets } from "@/constants/assets.constant";
+import { type Framework, frameworks } from "@/constants/framework.constant";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [currentFramework, setCurrentFramework] = useState<Framework>(
+    frameworks[0]
+  );
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const rotateFramework = () => {
+      setCurrentFramework(frameworks[currentIndex]);
+      currentIndex = (currentIndex + 1) % frameworks.length;
+    };
+    const intervalId = setInterval(rotateFramework, 2000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">Home</main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="fixed inset-0 transition-color delay-100 duration-700 opacity-25" />
+      <Image
+        src={assets.gradient}
+        width={1200}
+        height={1200}
+        role="presentation"
+        alt="gradient"
+        className="fixed inset-0 w-screen h-screen object-cover"
+      />
+      <div
+        className="fixed inset-0 opacity-30"
+        style={{
+          backgroundImage: `url(${assets.square})`,
+          backgroundSize: "30px",
+        }}
+      />
+    </main>
   );
 }
