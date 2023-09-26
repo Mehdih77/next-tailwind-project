bbf2a332a93ef096450dcac340bc7b1995f4869a
Mehdi <mehdi.havaei77@gmail.com>
2023-09-26 15:57:20 +0330

    implement timer

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import { assets } from "@/constants/assets.constant";
 import { type Framework, frameworks } from "@/constants/framework.constant";
 import { cn } from "@/utils/cn";
 import Image from "next/image";
+import Link from "next/link";
 import FrameworkRotation from "@/components/framework-rotation";
+import CountdownTimer from "@/components/countdown-timer";
 
 export default function Home() {
   const [currentFramework, setCurrentFramework] = useState<Framework>(
@@ -30,7 +32,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div
         className={cn(
-          "fixed inset-0 transition-color delay-100 duration-700 opacity-25",
+          "fixed inset-0 transition-colors delay-100 duration-700 opacity-25",
           {
             "bg-purple-300": currentFramework === "qwik",
             "bg-sky-300": currentFramework === "safari",
@@ -65,7 +67,6 @@ export default function Home() {
           !showBackground ? "opacity-100" : "opacity-0"
         )}
       />
-
       <div className="max-w-7xl mt-20 mx-auto">
         <div className="flex flex-col items-center relative z-10">
           <h1 className="text-5xl text-center leading-snug mb-12">
@@ -77,8 +78,45 @@ export default function Home() {
               height={50}
             />
             to <FrameworkRotation currentFramework={currentFramework} /> will{" "}
-            <span>never</span> be the same again
+            <span
+              className={cn("transition-colors duration-200", {
+                "text-purple-300": currentFramework === "qwik",
+                "text-sky-300": currentFramework === "safari",
+                "text-yellow-300": currentFramework === "chrome",
+                "text-teal-300": currentFramework === "tailwind",
+                "text-blue-300": currentFramework === "react",
+                "text-green-300": currentFramework === "vue",
+                "text-orange-300": currentFramework === "svelte",
+                "text-red-300": currentFramework === "mobile",
+                "text-neutral-300": currentFramework === "desktop",
+              })}>
+              NEVER
+            </span>{" "}
+            be the same again
           </h1>
+          <p className="mb-8">My Github Profile</p>
+          <div className="mb-8">
+            <Link href="https://github.com/Mehdih77" prefetch={false}>
+              <button
+                className={cn(
+                  "text-black px-6 py-3 rounded-md text-sm font-semibold transition-colors duration-200",
+                  {
+                    "bg-purple-300": currentFramework === "qwik",
+                    "bg-sky-300": currentFramework === "safari",
+                    "bg-yellow-300": currentFramework === "chrome",
+                    "bg-teal-300": currentFramework === "tailwind",
+                    "bg-blue-300": currentFramework === "react",
+                    "bg-green-300": currentFramework === "vue",
+                    "bg-orange-300": currentFramework === "svelte",
+                    "bg-red-300": currentFramework === "mobile",
+                    "bg-neutral-300": currentFramework === "desktop",
+                  }
+                )}>
+                Go To Gthub
+              </button>
+            </Link>
+          </div>
+          <CountdownTimer currentFramework={currentFramework} />
         </div>
       </div>
     </main>
