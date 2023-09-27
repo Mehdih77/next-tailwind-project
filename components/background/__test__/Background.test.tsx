f357cbf8cbaddd8c1a93764bdf3d9d464fb57e82
Mehdi <mehdi.havaei77@gmail.com>
2023-09-27 15:22:28 +0330

    refactor background test

--- components/background/__test__/Background.test.tsx
+++ components/background/__test__/Background.test.tsx
@@ -2,21 +2,32 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Background from "..";
 
+const mockFn = jest.fn();
+//! Base
+let setValue: React.Dispatch<React.SetStateAction<boolean>>;
+beforeEach(() => {
+  setValue = mockFn;
+});
+const baseOfTest = (bol: boolean) => {
+  React.useState = mockFn.mockImplementationOnce(() => [bol, setValue]);
+  render(<Background />);
+  const elm = screen.getByTestId("background");
+  return { elm };
+};
+
 describe("Background", () => {
-  let setValue: React.Dispatch<React.SetStateAction<boolean>>;
-  beforeEach(() => {
-    setValue = jest.fn();
-  });
   test("check className in first render", () => {
-    React.useState = jest.fn().mockImplementationOnce(() => [false, setValue]);
-    render(<Background />);
-    const elm = screen.getByTestId("background");
+    // React.useState = mockFn.mockImplementationOnce(() => [false, setValue]);
+    // render(<Background />);
+    // const elm = screen.getByTestId("background");
+    const { elm } = baseOfTest(false);
     expect(elm).toHaveClass("opacity-100");
   });
-  test("check className render the correct value", async () => {
-    React.useState = jest.fn().mockImplementationOnce(() => [true, setValue]);
-    render(<Background />);
-    const elm = screen.getByTestId("background");
+  test("check className render the correct value", () => {
+    // React.useState = mockFn.mockImplementationOnce(() => [true, setValue]);
+    // render(<Background />);
+    // const elm = screen.getByTestId("background");
+    const { elm } = baseOfTest(true);
     expect(elm).toHaveClass("opacity-0");
   });
 });
